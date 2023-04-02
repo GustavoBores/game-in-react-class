0404b0f8303e944decfc734d5b584e65536965e1
GustavoBores <binarymasterjobs@gmail.com>
2023-04-02 11:04:03 -0300

    Styled componen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React from "react"
+
+import { Main } from "./style"
 
 export default class App extends React.Component {
   constructor(props) {
@@ -8,7 +10,9 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-        
+        <Main>
+          
+        </Main>
       </>
     )
   }
